95538a4b795b25d2b89ec69454e85de1daa2b70b
Bemay <wlstmddn98@naver.com>
2025-04-06 15:58:05 +0900

    공통 응답 객체 및 예외처리 기본 세팅 완료

---
+++ cubco-common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package org.cubco.config;
+
+import org.cubco.common.exception.CustomException;
+import org.cubco.common.exception.ErrorCode;
+import org.cubco.common.response.CommonResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    // 커스텀 예외 처리
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<CommonResponse<Void>> handleCustomException(CustomException ex) {
+        ErrorCode errorCode = ex.getErrorCode();
+        return ResponseEntity
+                .status(errorCode.getHttpStatus())
+                .body(CommonResponse.fail(errorCode.getCode(), errorCode.getMessage()));
+    }
+
+    // 그 외 예외 처리
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<CommonResponse<Void>> handleException(Exception ex) {
+        ErrorCode errorCode = ErrorCode.INTERNAL_SERVER_ERROR;
+        return ResponseEntity
+                .status(errorCode.getHttpStatus())
+                .body(CommonResponse.fail(errorCode.getCode(), errorCode.getMessage()));
+    }
+}
