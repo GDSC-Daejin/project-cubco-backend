dc2dc40a9769e7064de8e19158402ec939b93533
june <0721hjp@naver.com>
2025-04-08 22:36:25 +0900

    feat: JWTutil 구현 - getUserId 메서드 추가 - getRole 메서드 추가 - isExpired 메서드 추가 - createToken 메서드 추가 - 클레임: iss, sub, iat, exp, roles

--- cubco-common/src/main/java/org/cubco/util/JWTutil.java
+++ cubco-common/src/main/java/org/cubco/util/JWTutil.java
@@ -1,4 +1,46 @@
 package org.cubco.util;
 
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
+@PropertySource("classpath:security.properties")
 public class JWTutil {
+
+    private SecretKey secretKey;
+
+    public JWTutil(@Value("${jwt.secret}")String secret){
+        this.secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
+    }
+
+    public Long getUserId(String token){
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("sub", Long.class);
+    }
+
+    public String getRole(String token){
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("roles", String.class);
+    }
+
+    public Boolean isExpired(String token){
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().getExpiration().before(new Date());
+    }
+
+    public String createToken(Long userId, String role, Long expiredms){
+        return Jwts.builder()
+                .claim("iss", "cubco")
+                .claim("sub", userId)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + expiredms))
+                .claim("roles", role)
+                .signWith(secretKey)
+                .compact();
+    }
+
 }
