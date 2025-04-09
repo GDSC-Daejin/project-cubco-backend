052c620fd947137c61e40dd32be4996dc37246de
june <0721hjp@naver.com>
2025-04-09 15:57:32 +0900

    feat: JwtAuthenticationFilter 구현 - 인증 실패 시 json 형식 반환(응답 형식 정해지면 추가 예정) - 토큰 검증 및 파싱 로직 구현 - SecurityConfig: JwtAuthenticationFilter 추가

--- cubco-common/src/main/java/org/cubco/config/SecurityConfig.java
+++ cubco-common/src/main/java/org/cubco/config/SecurityConfig.java
@@ -1,4 +1,47 @@
 package org.cubco.config;
 
+import org.cubco.filter.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
 public class SecurityConfig {
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    @Autowired
+    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration) {
+        this.authenticationConfiguration = authenticationConfiguration;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/v3/api-docs/**",
+                                "/swagger-resources/**"
+                        ).permitAll()
+                        .anyRequest().hasRole("ADMIN"))
+                .addFilterBefore(new JwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
 }
+
+
--- cubco-common/src/main/java/org/cubco/filter/JwtAuthenticationFilter.java
+++ cubco-common/src/main/java/org/cubco/filter/JwtAuthenticationFilter.java
@@ -1,4 +1,88 @@
 package org.cubco.filter;
 
-public class JwtAuthenticationFilter {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.cubco.util.JWTutil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+@PropertySource("classpath:security.properties")
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Autowired
+    JWTutil jwTutil;
+
+    @Value("${jwt.header}")
+    private String authorizationHeader;
+
+    private void handleAuthenticationError(HttpServletResponse response, String errorMessage) throws IOException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json;charset=UTF-8");
+        Map<String, Object> errorResponse = new HashMap<>();
+
+        /*
+        errorResponse.put("message", "인증 실패");
+        errorResponse.put("status", HttpStatus.UNAUTHORIZED);
+        .
+        .
+        */
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonResponse = objectMapper.writeValueAsString(errorResponse);
+        response.getWriter().write(jsonResponse);
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = request.getHeader(authorizationHeader);
+
+        if (token == null || !token.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        try {
+            token = token.substring(7);
+            Long userId = jwTutil.getUserId(token);
+            String role = jwTutil.getRole(token);
+
+            if (jwTutil.isExpired(token)) {
+                handleAuthenticationError(response, "토큰만료");
+                return;
+            }
+
+            List<GrantedAuthority> authorities = Collections.singletonList(new SimpleGrantedAuthority(role));
+            User authenticatedUser = new User(userId.toString(), "", authorities);
+
+            var authenticationToken = new UsernamePasswordAuthenticationToken(authenticatedUser, null, authorities);
+            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            handleAuthenticationError(response, e.getMessage());
+            return;
+        }
+
+        filterChain.doFilter(request, response);
+    }
 }
