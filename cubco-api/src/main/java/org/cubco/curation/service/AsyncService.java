471a31c4914000568a491b21b652341fb78c9140
Seung-9 <ssg9505fj22@naver.com>
2025-05-17 21:40:08 +0900

    [feat]: 큐레이션 이미지 및 태그 비동기 처리 구현

---
+++ cubco-api/src/main/java/org/cubco/curation/service/AsyncService.java
@@ -0,0 +1,39 @@
+package org.cubco.curation.service;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import org.cubco.curation.domain.Curation;
+import org.cubco.exception.EntityNotFoundException;
+import org.cubco.exception.ErrorCode;
+import org.cubco.image.service.ImageService;
+import org.cubco.curation.dto.request.TagCreateReq;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+@Transactional(readOnly = true)
+public class AsyncService {
+    private final ImageService imageService;
+    private final CurationTagService curationTagService;
+
+    @Transactional
+    public void createCurationImage(List<MultipartFile> images, Curation curation) {
+        if (images.isEmpty()) {
+            throw new EntityNotFoundException(ErrorCode.IMAGE_NOT_FOUND);
+        }
+        imageService.saveCurationImages(images, curation);
+    }
+
+    @Async
+    public void createCurationTags(List<TagCreateReq> tags, Curation curation) {
+        if (tags.isEmpty()) {
+            throw new EntityNotFoundException(ErrorCode.TAG_NOT_FOUND);
+        }
+        curationTagService.createCurationTag(tags, curation);
+    }
+}
