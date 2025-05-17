062942b04ec4f23b13ffc04633defec599b861b6
Seung-9 <ssg9505fj22@naver.com>
2025-05-17 21:39:34 +0900

    [chore]: Swagger JWT 인증 설정 및 MediaType 지원 확장

--- cubco-api/src/main/java/org/cubco/config/SwaggerConfig.java
+++ cubco-api/src/main/java/org/cubco/config/SwaggerConfig.java
@@ -1,11 +1,25 @@
 package org.cubco.config;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
+import java.util.ArrayList;
+
+@SecurityScheme(
+        name = "Authorization",
+        type = SecuritySchemeType.HTTP,
+        in = SecuritySchemeIn.HEADER,
+        bearerFormat = "JWT",
+        scheme = "Bearer"
+)
 @Configuration
 public class SwaggerConfig {
     @Bean
@@ -20,5 +34,13 @@ public class SwaggerConfig {
                 .title("CubCo Swagger")
                 .description("CubCo Backend REST API")
                 .version("1.0.0");
+    }
+
+    public SwaggerConfig(MappingJackson2HttpMessageConverter converter) {
+        var supportedMediaTypes = new ArrayList<>(converter.getSupportedMediaTypes());
+        supportedMediaTypes.add(new MediaType("application", "json"));
+        supportedMediaTypes.add(new MediaType("multipart", "form-data"));
+        supportedMediaTypes.add(new MediaType("application", "octet-stream"));
+        converter.setSupportedMediaTypes(supportedMediaTypes);
     }
 }
